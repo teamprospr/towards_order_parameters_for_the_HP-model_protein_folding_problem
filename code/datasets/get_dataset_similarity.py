5126725cd7ae30cb7b60d5c88cf0753d684a754d
Okke van Eck <okke.vaneck@bsc.es>
2026-03-03 12:08:17 +0100

    Added code for checking where similarity of datasets comes from

--- code/datasets/get_dataset_similarity.py
+++ code/datasets/get_dataset_similarity.py
@@ -1,14 +1,10 @@
-#! /usr/bin/env python3
-"""
-File:           get_dataset_similarity.py
-Description:    Check for similarity between the two generated datasets.
-"""
+#!/usr/bin/env python3
 
 from pathlib import Path
 import pandas as pd
 
 
-def get_dataset_sequences(dataset_path: Path, max_length: int=100):
+def get_dataset_sequences(dataset_path: Path, max_length: int=100) -> set:
     """Get dataset sizes with a limiter on length."""
     proteins = set()
     for p in dataset_path.iterdir():
@@ -16,12 +12,38 @@ def get_dataset_sequences(dataset_path: Path, max_length: int=100):
         and int(p.stem.split("_")[0]) <= max_length:
             # Get list of protein sequences.
             df = pd.read_csv(p)
-            proteins.update(df["sequence"].unique())
+            proteins.update(df["sequence"])
         if p.is_dir() and int(p.name) <= max_length:
             # Process dir recursively.
             proteins.update(get_dataset_sequences(p))
     return proteins
 
+
+def report_double_proteins(proteins_double: set):
+    """Report the sequence statistics of the double proteins."""
+    # Transform the set into a DataFrame and compute H-ratio.
+    df = pd.DataFrame(proteins_double, columns=["sequence"])
+    df["length"] = df["sequence"].str.len()
+    df["hratio"] = df["sequence"].apply(
+        lambda s: len([c for c in s if c == "H"])
+    ) / df["length"]
+
+    # Compute statistics over set of unique proteins.
+    print("Location of double proteins:")
+    lengths = sorted(df["length"].unique())
+
+    for length in lengths:
+        print(f"    Length {length}:")
+        df_length = df[df.length == length]
+        total_proteins = 0
+        hratios = sorted(df_length["hratio"].unique())
+
+        for hratio in hratios:
+            num_proteins = df_length[df_length.hratio == hratio]["sequence"].count()
+            total_proteins += num_proteins
+            print(f"        H-ratio {hratio}:\t{num_proteins}")
+
+        print(f"        Total:\t{total_proteins}")
 
 def main():
     """Entrypoint."""
@@ -42,7 +64,8 @@ def main():
     len_random = len(proteins_random)
     total_size = len_hratio + len_random
     num_unique = len(proteins_hratio.union(proteins_random))
-    num_double = len(proteins_hratio.intersection(proteins_random))
+    proteins_double = proteins_hratio.intersection(proteins_random)
+    num_double = len(proteins_double)
 
     # Report stats.
     print("Length H-ratio: ", len_hratio)
@@ -54,6 +77,10 @@ def main():
     print("============================")
     print(f"Percentage overlap: {num_double / total_size * 100:.2f} %")
     print(f"Percentage unique:  {num_unique / total_size * 100:.2f} %")
+    print("============================")
+
+    # Report where the double proteins reside.
+    report_double_proteins(proteins_double)
 
 
 if __name__ == "__main__":
